101444b3f43dcad595764e1fc3cdaf36898883b1
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 17:26:36 -0500

    ;s

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var featureCollection = require('turf-featurecollection')
+var featureCollection = require('turf-featurecollection');
 
 module.exports = function(collection, key, val) {
   var newFC = featureCollection([]);
   for(var i = 0; i < collection.features.length; i++) {
     if(collection.features[i].properties[key] === val) {
-      newFC.features.push(collection.features[i])
+      newFC.features.push(collection.features[i]);
     }
   }
-  return newFC
+  return newFC;
 }
