d66241de5f12ad33bced74d50ef88e531c82264b
Tom MacWright <tom@macwright.org>
2015-01-12 18:06:57 -0500

    Add jsdocs. Fixes #2

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var featureCollection = require('turf-featurecollection');
 
+/**
+ * Filter a FeatureCollection by a desired property key-value combination.
+ *
+ * @module turf/filter
+ * @param {FeatureCollection} features
+ * @param {string} key the key in a feature's properties property
+ * @param {string} value the desired value of that property
+ * @return {FeatureCollection} a filtered collection with only features that match.
+ * @example
+ * var trees = turf.featurecollection([
+ *  turf.point(1,2, {species: 'oak'}),
+ *  turf.point(2,1, {species: 'birch'}),
+ *  turf.point(3,1, {species: 'oak'}),
+ *  turf.point(2,2, {species: 'redwood'}),
+ *  turf.point(2,3, {species: 'maple'}),
+ *  turf.point(4,2, {species: 'oak'})
+ * ]);
+ * //=trees
+ * var filtered = turf.filter(trees, 'species', 'oak');
+ * //=filtered
+*/
 module.exports = function(collection, key, val) {
   var newFC = featureCollection([]);
   for(var i = 0; i < collection.features.length; i++) {
@@ -8,4 +29,4 @@ module.exports = function(collection, key, val) {
     }
   }
   return newFC;
-}
+};
