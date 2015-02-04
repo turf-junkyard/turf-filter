aa7a46cd6284c2d02c1640b00044758a14b80057
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-03 17:18:07 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -9,17 +9,69 @@ var featureCollection = require('turf-featurecollection');
  * @param {String} value the value of that property on which to filter
  * @return {FeatureCollection} a filtered collection with only features that match input `key` and `value`
  * @example
- * var features = turf.featurecollection([
- *  turf.point([-72.581777, 44.260875], {species: 'oak'}),
- *  turf.point([-72.570018, 44.260691], {species: 'birch'}),
- *  turf.point([-72.576284, 44.257925], {species: 'oak'}),
- *  turf.point([-72.56916, 44.254605], {species: 'redwood'}),
- *  turf.point([-72.581691, 44.24858], {species: 'maple'}),
- *  turf.point([-72.583837, 44.255773], {species: 'oak'})
- * ]);
+ * var features = {
+ *   "type": "FeatureCollection",
+ *   "features": [
+ *     {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "oak"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.581777, 44.260875]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "birch"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.570018, 44.260691]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "oak"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.576284, 44.257925]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "redwood"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.56916, 44.254605]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "maple"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.581691, 44.24858]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "species": "oak"
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [-72.583837, 44.255773]
+ *       }
+ *     }
+ *   ]
+ * };
  *
- * var key = 'species';
- * var value = 'oak';
+ * var key = "species";
+ * var value = "oak";
  *
  * var filtered = turf.filter(features, key, value);
  *
